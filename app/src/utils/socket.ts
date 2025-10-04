a13d9b4f0a9937150c94f87245ffbe42037ebbed
Francesco Signoretti <signorettif@gmail.com>
2025-10-04 19:07:00 -0400

    fix: use env variable for socket uri

--- app/src/utils/socket.ts
+++ app/src/utils/socket.ts
@@ -1,10 +1,3 @@
 export const getSocketUri = (): string => {
-  if (
-    window.location.hostname === 'localhost' ||
-    window.location.hostname === '127.0.0.1'
-  ) {
-    return 'ws://localhost:3000/socket';
-  } else {
-    return 'wss://crowdpad-server.fly.dev/socket';
-  }
+  return import.meta.env.VITE_WEBSOCKET_URL || 'ws://localhost:3000/socket';
 };
