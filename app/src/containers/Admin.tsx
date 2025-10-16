e1c7795703765c6867bc3013240ed932e412b5c0
Lorenzo Signoretti <lorenzosigno@gmail.com>
2025-10-16 15:03:07 -0500

    simplify layout of pages

--- app/src/containers/Admin.tsx
+++ app/src/containers/Admin.tsx
@@ -8,36 +8,32 @@ import type { Config } from '../types';
 
 export const Admin = () => {
   const [initialConfig, setInitialConfig] = useState<Config | undefined>(
-    undefined,
+    undefined
   );
   const [error, setError] = useState<Error | undefined>(undefined);
   const loadingInitialConfig = !initialConfig && !error;
 
   useEffect(() => {
     getConfig()
-      .then(configResponse => {
+      .then((configResponse) => {
         setInitialConfig(configResponse);
       })
-      .catch(err => setError(err));
+      .catch((err) => setError(err));
   }, []);
 
   return (
-    <div className="min-h-screen bg-gray-100">
-      <div className="container mx-auto p-4">
-        <div className="flex flex-col gap-6 lg:flex-row">
-          <div className="flex-1 rounded-lg bg-white p-6 shadow-lg">
-            {loadingInitialConfig ? (
-              'Loading...'
-            ) : error ? (
-              `There was an error: ${error.message}`
-            ) : initialConfig ? (
-              <UpdateConfigForm initialConfig={initialConfig} />
-            ) : (
-              'No configuration found.'
-            )}
-          </div>
-        </div>
+    <main className="container mx-auto flex flex-col gap-6 p-6 lg:flex-row">
+      <div className="flex-1 rounded-lg bg-white p-6 shadow-lg">
+        {loadingInitialConfig ? (
+          'Loading...'
+        ) : error ? (
+          `There was an error: ${error.message}`
+        ) : initialConfig ? (
+          <UpdateConfigForm initialConfig={initialConfig} />
+        ) : (
+          'No configuration found.'
+        )}
       </div>
-    </div>
+    </main>
   );
 };
